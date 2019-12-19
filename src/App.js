b995907d67cc1ec0dc334d12db55d1a83415ce01
Yo shinzato <s19008@std.it-college.ac.jp>
2019-12-19 16:27:47 +0900

    サイクル

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+class App extends Component {
+  
+  constructor (props) {
+    super(props)
+    console.log('constructor')
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount () {
+    console.log('componentDidMount')
+  }
+  
+  componentWillUpdate () {
+    console.log('componentWillUpdate')
+  }
+
+  componentDidUpdate () {
+    console.log('componentDidUpdate')
+  }
+
+  componentWillUnmount () {
+    console.log('componentWillUnmount')
+  }
+  
+  render () {
+    console.log('render')
+    const setStateHandler = (e) => {
+      console.log('* call setState()')
+      this.setState({r: Math.random()})
+    }
+  
+      return (
+          <div>
+              <button onClick={setStateHandler}>
+                  setState</button>
+          </div>
+      )
+  }
 }
 
-export default App;
+export default App
